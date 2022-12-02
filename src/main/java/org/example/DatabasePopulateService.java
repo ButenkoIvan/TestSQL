3ec8d5fcf1c6911387aa398c39358d6ad5c9a0d1
Іван <butenkoivana898@gmail.com>
2022-12-02 12:38:09 +0200

    ок

---
+++ src/main/java/org/example/DatabasePopulateService.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabasePopulateService {
+    public static void main(String[] args) throws IOException, SQLException {
+        String insertFileName = new Prefs().getPref(Prefs.INSERT_SQL_FILE_PATH);
+        String sql = Files.readString(Paths.get(insertFileName));
+
+        Database database = Database.getInstance();
+
+        Connection connection = database.getConnection();
+        Statement st = connection.createStatement();
+        st.executeUpdate(sql);
+    }
+}
